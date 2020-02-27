de1b63ce2980982cb6274ca05cde2e136bd703d5
PierreGuilmin <pierreguilmin@gmail.com>
2020-02-27 16:44:12 +0100

    Remove useless ConversationStage entry and reorder entries

--- foodshare/handlers/cook_conversation/__init__.py
+++ foodshare/handlers/cook_conversation/__init__.py
@@ -2,10 +2,9 @@ from enum import Enum, auto
 
 
 class ConversationStage(Enum):
-    SELECTING_DATE = auto()
     TYPING = auto()
+    SELECTING_DATE = auto()
     SELECTING_DATE_CALENDAR = auto()
-    NUMBER_SELECTION = auto()
     SELECTING_HOUR = auto()
     SELECTING_NUMBER = auto()
     SELECTING_COST = auto()
