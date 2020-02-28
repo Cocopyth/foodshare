208ade06498ba5a499906268c8acc5ea67303400
PierreGuilmin <pierreguilmin@gmail.com>
2020-02-28 10:08:52 +0100

    Minor fix to previous commit ed4b921

--- foodshare/keyboards/__init__.py
+++ foodshare/keyboards/__init__.py
@@ -1,9 +1,12 @@
+from importlib import import_module
 import sys
 from pathlib import Path
 
 # let's add the git submodule to our Python path
 # Note: `Path(__file__).resolve().parent` is equivalent to
 # `os.path.dirname(os.path.abspath(__file__))`
-sys.path.append(str(Path(__file__).resolve().parent / 'calendar-telegram'))
+sys.path.append(str(Path(__file__).resolve().parent))
 
-import telegramcalendar as telegram_calendar
+# the following is necessary to import a file name with a dash
+# Note: `import_module('foo.bar')` is equivalent to `import foo.bar`
+telegram_calendar = import_module('calendar-telegram.telegramcalendar')
